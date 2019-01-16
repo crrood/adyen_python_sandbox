b8309e1ceab6c5a50f033e057076b20efa667744
crrood <crrood@gmail.com>
2019-01-16 12:11:27 -0600

    get rid of some of that sweet, sweet lint

--- scripts/common.js
+++ scripts/common.js
@@ -1,35 +1,43 @@
 // constants
-var FORM_ENCODED_HEADER = { "Content-Type": "application/x-www-form-urlencoded" };
-var SERVER_URL = "cgi-bin/submit.py";
+const FORM_ENCODED_HEADER = { "Content-Type": "application/x-www-form-urlencoded" };
+const SERVER_URL = "http://localhost:8000/cgi-bin/submit.py";
 
 // wrapper to send requests to server
 function AJAXPost(path, callback, headers = FORM_ENCODED_HEADER, params = {}, method = "POST") {
-	var request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 	request.open("POST", path, true);
 	request.onreadystatechange = callback;
 
-	for (var key in headers) {
+	for (let key in headers) {
 		request.setRequestHeader(key, headers[key]);
 	}
 
 	request.send(params);
-};
+}
 
 // pulls parameters from HTML form and sends to server
 // accepts an optional object of parameters to add
 function buildFormURL(customParams = null) {
-	var inputParams = document.querySelectorAll("input[type='text'], input[type='hidden']");
+	let inputParams = document.querySelectorAll("input[type='text'], input[type='hidden']");
 
 	// Get request details from html form
-	var formString = "";
-	for (var param of inputParams) {
+	let formString = "";
+	for (let param of inputParams) {
 		formString = formString + param.name + "=" + param.value + "&";
 	}
 
 	// Add custom parameters from function call
-	for (var key in customParams) {
+	for (let key in customParams) {
 		formString = formString + key + "=" + customParams.key + "&";
 	}
 
 	return encodeURI(SERVER_URL + "?" + formString);
+}
+
+// generate UUID
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
