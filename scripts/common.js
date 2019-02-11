c2adb83bb083fc90d678dcded05b9ea9de1a4c28
crrood <crrood@gmail.com>
2019-02-11 13:44:55 -0800

    move output to common.js

--- scripts/common.js
+++ scripts/common.js
@@ -50,7 +50,64 @@ export function buildFormURL(customParams = null) {
 	return encodeURI(SERVER_URL + "?" + formString);
 }
 
-// generate UUID
+// utility to output to web page
+export function output(text, title = null, subtitle = null, indentation = 4) {
+	const containerEl = document.querySelector("#output");
+
+	// add a title if param is present
+	if (title) {
+		const titleEl = document.createElement("div");
+		titleEl.innerHTML = "--------------- " + title + " ---------------";
+		containerEl.append(titleEl);
+	}
+
+	// add a subtitle, usually endpoint or SDK method
+	if (subtitle) {
+		const subtitleContainer = document.createElement("pre");
+		const subtitleEl = document.createElement("code");
+		subtitleContainer.appendChild(subtitleEl);
+		subtitleEl.innerHTML = subtitle;
+		containerEl.append(subtitleEl);
+	}
+
+	// format output
+	if (typeof(text) === "object") {
+		try {
+			// indent JSON object
+			text = JSON.stringify(text, null, indentation);
+		}
+		catch(e) {
+			// convert object to string
+			text = text.toString();
+		}
+	}
+	else if (typeof(text) === "string") {
+		try {
+			// try to convert text to JSON
+			text = JSON.stringify(JSON.parse(text), null, indentation);
+		}
+		catch(e) {
+			// do nothing; sometimes text is just text - Lao Tzu
+		}
+	}
+
+	// remove extra backslashes from overzealous URL encoding
+	text = text.replace(/\\/g, "");
+
+	// create element to be added
+	const outputContainer = document.createElement("pre");
+	const outputEl = document.createElement("code");
+	outputContainer.appendChild(outputEl);
+	outputEl.innerHTML = text;
+	containerEl.append(outputContainer);
+}
+
+// this shouldn't be necessary.. I must be building the response wrong somehow
+export function sanitizeJSON(data) {
+	return JSON.parse(data.replace(/'/g, '"').replace(/"\{/g, "{").replace(/\}"/g, "}"));
+}
+
+// generate UUID for 3DS2 advanced flow
 export function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
